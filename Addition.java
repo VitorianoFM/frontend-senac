563dc957c8b7fd98f5c2ced0e2ab40863a470cf6
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2024-08-14 11:17:23 -0300

    Add files via upload
    Incluído a 3 variável na adição.

--- Addition.java
+++ Addition.java
@@ -10,12 +10,15 @@ public class Addition // criado (declarado) a classe Addition (adição)
          JOptionPane.showInputDialog("Digite o primeiro número inteiro"); // exibe uma caixa com texto/parâmetro solicitando a entrada de dados pelo usuário em um campo de texto, e botões OK e Cancel
       String secondNumber = // instância a classe String para receber o segundo número || "=" é igual operador de atribuição
           JOptionPane.showInputDialog("Digite o segundo número inteiro"); // exibe uma caixa com texto/parâmetro solicitando a entrada de dados pelo usuário em um campo de texto, e botões OK e Cancel
+      String thirdNumber = // instância a classe String para receber o terceiro número || "=" é igual operador de atribuição
+          JOptionPane.showInputDialog("Digite o terceiro número inteiro"); // exibe uma caixa com texto/parâmetro solicitando a entrada de dados pelo usuário em um campo de texto, e botões OK e Cancel
 
       // converter entradas de String (texto) em valores int para uso no cálculo
       int number1 = Integer.parseInt(firstNumber); // converte a entrada da String firstNumber para uma variável tipo (número) inteiro number1 || "=" é igual operador de atribuição
       int number2 = Integer.parseInt(secondNumber); // converte a entrada da String secondNumber para uma variável tipo (número) inteiro number2 || "=" é igual operador de atribuição
+      int number3 = Integer.parseInt(thirdNumber); // converte a entrada da String secondNumber para uma variável tipo (número) inteiro number3 || "=" é igual operador de atribuição
 
-      int sum = number1 + number2; // realiza a soma dos dois números inteiros, somando o number1 + number2 resultando na variável sum
+      int sum = number1 + number2 + number3; // realiza a soma dos dois números inteiros, somando o number1 + number2 + number3 resultando na variável sum
 
       // exibir resultado em uma caixa de diálogo de mensagem JOptionPane
       JOptionPane.showMessageDialog(null, "A soma é " + sum, // mostra o resultado da soma (sum) após a impressão do texto || o pacote showMessageDialog já converte a variável posterior quando é concatenado com uma String || "," = separador de parâmetro
