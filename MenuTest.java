45bd47ec07095d8275f51959b55d8e5f3c8b5bd2
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2024-09-11 11:54:32 -0300

    Inclusão de cor e fonte

--- MenuTest.java
+++ MenuTest.java
@@ -1,7 +1,7 @@
 // Testando MenuFrame.
 import javax.swing.JFrame; // javax.swing: É um pacote do Java que contém classes para criar interfaces gráficas com o usuário (GUIs), como janelas, botões, campos de texto e muito mais. JFrame: É uma classe que representa uma janela básica em uma aplicação Java Swing. Ela serve como a "tela" principal onde você coloca outros componentes da interface.
 
-public class MenuTest // Declarar uma nova classe pública chamada MenuTest
+public class MenuTest // Declarar uma nova classe pública chamada MenuTest.
 {
    public static void main(String[] args) // criado (declarado) o método executor main || "[]" = matriz
    { 
