0aefce803f97f5612b21558598738fbd112078e3
Vitoriano Ferrero Martin Jr <victorianojr@gmail.com>
2024-09-19 11:51:47 -0300

    Documentação parcial TelaDePesquisa.java
    Documentação parcial TelaDePesquisa.java e criação do NavegadorDeRegistro.java

--- TelaDePesquisa.java
+++ TelaDePesquisa.java
@@ -1,52 +1,55 @@
 import java.awt.*; // Importa todos os componentes do módulo AWT.
 import java.awt.event.*; // import java.awt.event.*;
-import java.sql.*; // Importa todos os caomponentes do modulos sql para usar o banco de dados do MySQL.
+// import java.sql.*; // Importa todos os caomponentes do modulos sql para usar o banco de dados do MySQL.
 import javax.swing.*; // Importa todos os componentes do módulo Swing.
 
 // Classe TelaDeLogin que herda de JFrame para criar uma interface gráfica de login.
 public class TelaDePesquisa extends JFrame 
 {   
    // Declaração dos componentes da interface gráfica.
-   private final JLabel lblId; // Rótulo para o campo de nome.
-   private final JTextField txtId; // Campo de texto para o nome.
+   public static JLabel lblId; // Rótulo para o campo de nome.
+   public static JTextField txtId; // Campo de texto para o nome.
    
-   private final JLabel lblNome; // Rótulo para o campo de nome.
-   private final JTextField txtNome; // Campo de texto para o nome.
+   public static JLabel lblNome; // Rótulo para o campo de nome.
+   public static JTextField txtNome; // Campo de texto para o nome.
 
-   private final JLabel lblEmail; // Rótulo para o campo de email.
-   private final JTextField txtEmail; // Campo de texto para o email.
+   public static JLabel lblEmail; // Rótulo para o campo de email.
+   public static JTextField txtEmail; // Campo de texto para o email.
 
-   private final JLabel lblPesquisa; // Rótulo para o campo de nome.
-   private final JTextField txtPesquisa; // Campo de texto para o email.
+   public static JLabel lblPesquisa; // Rótulo para o campo de nome.
+   public static JTextField txtPesquisa; // Campo de texto para o email.
 
-   private final JButton btnBuscar; // Botão para submeter o cadastro.
-   private final JButton btnComeco; // Botão para submeter o cadastro.
-   private final JButton btnVoltar; // Botão para submeter o cadastro.
-   private final JButton btnAvancar; // Botão para submeter o cadastro.
-   private final JButton btnFim; // Botão para submeter o cadastro.
+   public static JButton btnPesquisar; // Botão para submeter o cadastro.
+   public static JButton btnComeco; // Botão para submeter o cadastro.
+   public static JButton btnVoltar; // Botão para submeter o cadastro.
+   public static JButton btnAvancar; // Botão para submeter o cadastro.
+   public static JButton btnFim; // Botão para submeter o cadastro.
 
-   private final JLabel lblNotificacoes; // Rótulo para exibir notificações ou mensagens ao usuário.
+   public static JLabel lblNotificacoes; // Rótulo para exibir notificações ou mensagens ao usuário.
+
+   public static int tamanhoInputs = 20;
+   public static String txtUsuario = "";
 
    // Construtor da classe TelaDeCadastro, onde os componentes são configurados e adicionados à janela.
    public TelaDePesquisa() 
    {
       super("Tela de Pesquisa"); // Define o título da janela.
-      setLayout(new GridLayout(7, 1, 5, 5)); // Define o layout da janela como GridLayout.
+      setLayout(new GridLayout(7,1,5,5)); // Define o layout da janela como GridLayout.
 
       JPanel linha_lblPesquisa = new JPanel(new GridLayout(1, 2));
 
       lblPesquisa = new JLabel("Pesquisa:", SwingConstants.CENTER); // Cria um rótulo com o texto "Pesquisa:".
       linha_lblPesquisa.add(lblPesquisa);
       
-      btnBuscar = new JButton("🔍"); // Cria um botão com o texto "Cadastrar".
-      btnBuscar .setToolTipText("Pesquisar");
-      linha_lblPesquisa.add(btnBuscar);
+      btnPesquisar = new JButton("🔍"); // Cria um botão com o texto "Cadastrar".
+      btnPesquisar.setToolTipText("Pesquisar");
+      linha_lblPesquisa.add(btnPesquisar);
 
       add(linha_lblPesquisa);
 
       JPanel linha_inputPesquisa = new JPanel(new GridLayout(1, 1));
 
-      txtPesquisa = new JTextField(25); // Cria um campo de texto com largura de 25 colunas.
+      txtPesquisa = new JTextField(tamanhoInputs); // Cria um campo de texto com largura de 20 colunas.
       linha_inputPesquisa.add(txtPesquisa); // Adiciona o campo de texto à janela.
 
       add(linha_inputPesquisa);
@@ -56,92 +59,92 @@ public class TelaDePesquisa extends JFrame
       lblId = new JLabel("ID:", SwingConstants.RIGHT); // Cria um rótulo com o texto "ID".
       linha_id.add(lblId); // Adiciona o rótulo à janela.
 
-      txtId = new JTextField(25); // Cria um campo de texto com largura de 25 colunas.
+      txtId = new JTextField(tamanhoInputs); // Cria um campo de texto com largura de 20 colunas.
+      txtId.setEnabled(false);
       linha_id.add(txtId); // Adiciona o campo de texto à janela.
 
-      lblNome = new JLabel("Nome:", SwingConstants.RIGHT); // Cria um rótulo com o texto "Nome".
-      add(lblNome); // Adiciona o rótulo à janela.
+      add(linha_id);
 
-      txtNome = new JTextField(25); // Cria um campo de texto com largura de 25 colunas.
-      add(txtNome); // Adiciona o campo de texto à janela.
+      JPanel linha_nome = new JPanel(new GridLayout(1, 2));
+
+      lblNome = new JLabel("Nome:", SwingConstants.RIGHT); // Cria um rótulo com o texto "Nome".
+      linha_nome.add(lblNome); // Adiciona o rótulo à janela.
+
+      txtNome = new JTextField(tamanhoInputs); // Cria um campo de texto com largura de 20 colunas.
+      txtNome.setEditable(false);
+      linha_nome.add(txtNome); // Adiciona o campo de texto à janela.
+
+      add(linha_nome);
+
+      JPanel linha_email = new JPanel(new GridLayout(1, 2));
 
       lblEmail = new JLabel("E-mail:", SwingConstants.RIGHT); // Cria um rótulo com o texto "E-mail".
-      add(lblEmail); // Adiciona o rótulo à janela.
+      linha_email.add(lblEmail); // Adiciona o rótulo à janela.
 
-      txtEmail = new JTextField(25); // Cria um campo de texto com largura de 25 colunas.
-      add(txtEmail); // Adiciona o campo de texto à janela.
+      txtEmail = new JTextField(tamanhoInputs); // Cria um campo de texto com largura de 20 colunas.
+      txtEmail.setEditable(false);
+      linha_email.add(txtEmail); // Adiciona o campo de texto à janela.
+
+      add(linha_email);
+
+      JPanel linha_botoes = new JPanel(new GridLayout(1, 4));
       
-      btnComeco = new JButton("<<"); // Cria um botão com o texto "Cadastrar".
-      add(btnComeco); // Adiciona o botão à janela.
+      btnComeco = new JButton("<<"); // Cria um botão com o texto "<<".
+      linha_botoes.add(btnComeco); // Adiciona o botão à janela.
 
-      btnVoltar = new JButton("<"); // Cria um botão com o texto "Cadastrar".
-      add(btnVoltar); // Adiciona o botão à janela.
+      btnVoltar = new JButton("<"); // Cria um botão com o texto "<".
+      linha_botoes.add(btnVoltar); // Adiciona o botão à janela.
 
-      btnAvancar = new JButton(">"); // Cria um botão com o texto "Cadastrar".
-      add(btnAvancar); // Adiciona o botão à janela.
+      btnAvancar = new JButton(">"); // Cria um botão com o texto ">".
+      linha_botoes.add(btnAvancar); // Adiciona o botão à janela.
 
-      btnFim = new JButton(">>"); // Cria um botão com o texto "Cadastrar".
-      add(btnFim); // Adiciona o botão à janela.
+      btnFim = new JButton(">>"); // Cria um botão com o texto ">>".
+      linha_botoes.add(btnFim); // Adiciona o botão à janela.
+
+      add(linha_botoes);
+
+      JPanel linha_notificacoes = new JPanel(new GridLayout(1, 1));
 
       //add(new JLabel("     ")); // Adiciona um rótulo vazio para espaçamento.
       lblNotificacoes = new JLabel("Notificações:", SwingConstants.CENTER); // Cria um rótulo centralizado para notificações.
-      add(lblNotificacoes); // Adiciona o rótulo à janela.
+      linha_notificacoes.add(lblNotificacoes); // Adiciona o rótulo à janela.
 
-    //   btnCadastrar.addActionListener( // Configura um botão para que, ao ser clicado, ele execute uma determinada ação.
-    //      new ActionListener() { // Definir o que acontece quando um evento de ação é gerado para aquele componente.
-    //          @Override // Uma classe herda características de outra classe (classe pai), ela pode "reescrever" ou "sobrepor" métodos que já existem na classe pai. Essa ação é chamada de sobrescrita de método.
-    //          public void actionPerformed(ActionEvent event) { // Define um método que será executado quando um evento de ação acontecer em um componente da interface gráfica.
-    //              if (txtNome.getText().trim().length() <= 3) { // Verifica se um campo de texto, identificado por txtNome, contém algum valor válido antes de prosseguir com um cadastro.
-    //                  lblNotificacoes.setText(setHtmlFormat("É necessário digitar um Nome para o cadastro. Por favor, digite um nome e tente novamente."));
-    //                  txtNome.requestFocus();
-    //                  return;
-    //              }
+      add(linha_notificacoes);
 
-    //              if (txtEmail.getText().trim().length() <= 5) { // Verifica se um campo de texto, identificado por txtEmail, contém algum valor válido antes de prosseguir com um cadastro.
-    //                  lblNotificacoes.setText(setHtmlFormat("É necessário digitar um Email para o cadastro. Por favor, digite um Email e tente novamente."));
-    //                  txtEmail.requestFocus();
-    //                  return;
-    //              }
+      btnPesquisar.addActionListener(
+         new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent event) {
+                 if (txtPesquisa.getText().trim().length() <= 0) {
+                     lblNotificacoes.setText(setHtmlFormat("Por favor, digite algo e tente novamente."));
+                     txtPesquisa.requestFocus();
+                     return;
+                 }
 
-                //  if (String.valueOf(txtSenha.getPassword()).trim().length() <= 7) { // Verifica se um campo de texto, identificado por txtSenha, contém algum valor válido antes de prosseguir com um cadastro.
-                //      lblNotificacoes.setText(setHtmlFormat("É necessário digitar uma Senha com 8 digitos para o cadastro. Por favor, digite uma Senha e tente novamente."));
-                //      txtSenha.requestFocus();
-                //      return;
-                //  }
+                 NavegadorDeRegistro.pesquisar();
+             }
+         }
+     );
 
-    //              try { // O programa está tentando se conectar ao banco de dados MySQL, executar uma consulta SQL para buscar registros com um determinado endereço de email e armazenar o resultado em um objeto ResultSet.
-    //                  Connection conexao = MySQLConnector.conectar();
-    //                  String strSqlEmail = "select * from `db_senac`.`tbl_senac` where `email` = '" + txtEmail.getText() + "';";
-    //                  Statement stmSqlEmail = conexao.createStatement();
-    //                  ResultSet rstSqlEmail = stmSqlEmail.executeQuery(strSqlEmail);
-    //                  if (rstSqlEmail.next()) { // Verifica a existência de um usuário com determinado e-mail em um banco de dados.
-    //                      lblNotificacoes.setText(setHtmlFormat("Ops! Já existe um usuário utilizando este email. Por favor, digite outro email e tente novamente."));
-    //                  } else { // Verifica se o login é válido. Caso não seja, ele permite que o usuário realize um cadastro. Os dados do cadastro são inseridos em um banco de dados e uma mensagem de confirmação é exibida para o usuário.
-    //                      lblNotificacoes.setText(setHtmlFormat("Login liberado para cadastro."));
-    //                      String strSqlCadastrar = "insert into `db_senac`.`tbl_senac` (`nome`, `email`, `senha`) values ('" + txtNome.getText() + "', '" + txtEmail.getText() + "', '" + String.valueOf(txtSenha.getPassword()) + "');";
-    //                      // System.out.println(strSqlCadastrar);
-    //                      Statement stmSqlCadastrar = conexao.createStatement();
-    //                      stmSqlCadastrar.addBatch(strSqlCadastrar);
-    //                      stmSqlCadastrar.executeBatch();
-    //                      lblNotificacoes.setText(setHtmlFormat("Cadastro realizado com sucesso"));
-    //                  }
-    //                  stmSqlEmail.close(); // Encerra a comunicação com o banco de dados que estava sendo utilizada para realizar as operações relacionadas ao envio de e-mails.
-    //              } catch (Exception e) { // Se der algum problema ao cadastrar, mostrar uma mensagem de erro para o usuário e imprimir uma mensagem feinha para o programador.
-    //                  lblNotificacoes.setText(setHtmlFormat("Não foi possível prosseguir com o cadastro! Por favor, verifique e tente novamente."));
-    //                  System.err.println("Erro: " + e);
-    //              }
-    //          }
-    //      }
-    //  );
-   
+     txtPesquisa.addKeyListener(
+      new KeyAdapter() {
+         @Override
+         public void keyReleased(KeyEvent e) {
+            if (txtPesquisa.getText().trim().equals(txtUsuario) == false) {
+               btnPesquisar.setEnabled(true);
+            } else {
+               btnPesquisar.setEnabled(false);
+            }
+         }
+      }
+     );
+
       setSize(270, 300); // Define o tamanho da janela para 270 pixels de largura e 220 pixels de altura.
       setVisible(true); // Torna a janela visível.
-      txtPesquisa.requestFocus();
-
-      
+      txtPesquisa.requestFocus();  
    }
    
-   private String setHtmlFormat(String strTexto) { // O método setHtmlFormat recebe uma string como argumento e retorna essa string formatada como um documento HTML básico.
+   public static String setHtmlFormat(String strTexto) { // O método setHtmlFormat recebe uma string como argumento e retorna essa string formatada como um documento HTML básico.
       return "<html><body>" + strTexto + "</body></html>";
    }
    // Método main, ponto de entrada do programa.
